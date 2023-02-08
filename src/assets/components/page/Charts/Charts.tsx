9bb6e621fdb3f690b582fc1942e019aa84c2456a
shtamtails <maximyudin1@gmail.com>
2023-02-09 02:24:08 +0300

    BLOC-4/2

--- src/assets/components/page/Charts/Charts.tsx
+++ src/assets/components/page/Charts/Charts.tsx
@@ -1,36 +1,155 @@
+import { useEffect, useState } from "react";
 import { Chart } from "../../reusable/Chart/Chart";
 
+type Tcryptocurrencies = {
+  name: string;
+  visibleName: string;
+  logo: string;
+  price: string | null;
+  data: { uv: number }[];
+  diff: string | number;
+}[];
+
 export const Charts = () => {
-  const data: { uv: number }[] = [
+  const getRandomData = () => {
+    const data: { uv: number }[] = [
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+      {
+        uv: Math.random() * 100,
+      },
+    ];
+    return data;
+  };
+
+  const getPrice = async (name: string): Promise<string> => {
+    type avgPrice = { mins: number; price: string };
+    const response = await fetch(`https://api.binance.com/api/v3/avgPrice?symbol=${name}`);
+    const price: avgPrice = await response.json();
+    const tranformedPrice = +price.price;
+    return `${tranformedPrice.toFixed(2)}$`;
+  };
+
+  const cryptocurrenciesDefault: Tcryptocurrencies = [
     {
-      uv: 2300,
+      name: "BTCUSDT",
+      visibleName: "BTC",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 3000,
+      name: "ETHUSDT",
+      visibleName: "ETH",
+      logo: "https://www.citypng.com/public/uploads/preview/ethereum-eth-round-logo-icon-png-11662225468t3ckimsgp8.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 2000,
+      name: "DOGEUSDT",
+      visibleName: "DOGE",
+      logo: "https://www.citypng.com/public/uploads/preview/-31621003493w3kgccg24w.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 2780,
+      name: "SOLUSDT",
+      visibleName: "SOL",
+      logo: "https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 1890,
+      name: "BNBUSDT",
+      visibleName: "BNB",
+      logo: "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/512/Binance-Coin-BNB-icon.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 2390,
+      name: "XRPUSDT",
+      visibleName: "XRP",
+      logo: "https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/512/Ripple-XRP-icon.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
     {
-      uv: 4649.3,
+      name: "SHIBUSDT",
+      visibleName: "SHIB",
+      logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Shiba_Inu_coin_logo.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
+    },
+    {
+      name: "LTCUSDT",
+      visibleName: "LTC",
+      logo: "https://cryptologos.cc/logos/litecoin-ltc-logo.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
+    },
+    {
+      name: "TRXUSDT",
+      visibleName: "LTC",
+      logo: "https://stakingcrypto.info/static/assets/coins/tron-logo.png",
+      price: null,
+      data: getRandomData(),
+      diff: Math.floor(Math.random() * (100 - -100) + -100).toFixed(2),
     },
   ];
+
+  const [cryptocurrencies, setCryptocurrencies] = useState<Tcryptocurrencies>(cryptocurrenciesDefault);
+
+  const updateCryptocurrencies = async () => {
+    const updatedCryptocurrencies = [...cryptocurrencies];
+    for (let i = 0; i < updatedCryptocurrencies.length; i++) {
+      const el = updatedCryptocurrencies[i];
+      el.price = await getPrice(el.name);
+      updatedCryptocurrencies[i] = el;
+      setCryptocurrencies(updatedCryptocurrencies);
+    }
+  };
+
+  useEffect(() => {
+    updateCryptocurrencies();
+  }, []);
+
   return (
     <div className="charts">
-      <Chart name="BTC" price="1$" diff={3.94} data={data} />
-      <Chart name="ETH" price="1$" diff={3.94} data={data} />
-      <Chart name="DOGE" price="1$" diff={3.94} data={data} />
-      <Chart name="USDT" price="1$" diff={3.94} data={data} />
-      <Chart name="SOL" price="1$" diff={3.94} data={data} />
+      {cryptocurrencies.map((el, i) => (
+        <Chart
+          name={el.visibleName}
+          price={el.price}
+          diff={el.diff}
+          data={el.data}
+          img={el.logo}
+          key={i}
+          visible={!!el.price}
+        />
+      ))}
     </div>
   );
 };
--- src/assets/components/reusable/ActionIcon/ActionIcon.tsx
+++ src/assets/components/reusable/ActionIcon/ActionIcon.tsx
@@ -15,11 +15,12 @@ export const ActionIcon: React.FC<ActionIconProps> = (props) => {
   const classNames: string[] = ["action-icon"];
   props.className && classNames.push(props.className);
   props.type && classNames.push(`action-icon-${props.type}`);
+  props.borderRadius && classNames.push(`border-${props.borderRadius}`);
 
   return (
     <a href={props.src} className={classNames.join(" ")} onClick={props.onClick}>
       {props.icon && props.icon}
-      {props.img && <img src={props.img} />}
+      {props.img && <img src={props.img} className={props.borderRadius && `border-${props.borderRadius}`} />}
     </a>
   );
 };
