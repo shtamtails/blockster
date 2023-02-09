cf64a4bfafdedc266e6dbfb4546c2ed863fb07c8
shtamtails <maximyudin1@gmail.com>
2023-02-09 21:14:51 +0300

    BLOC-8

---
+++ src/assets/components/page/AirdropsCalendar/AirdropsCalendar.tsx
@@ -0,0 +1,46 @@
+import { Title } from "../../reusable/Title/Title";
+
+export const AirdropsCalendar = () => {
+  return (
+    <>
+      <Title text="Airdrops calendar" line />
+      <div className="airdrops-calendar">
+        <div className="box">
+          <div className="airdrop-date">
+            <span>3</span> Feb 2023
+          </div>
+          <div className="airdrop-name">TopGoal x CoinMarketCap 3rd NFTs Airdrop Event</div>
+          <div className="airdrop-amount">3 000 NFT</div>
+        </div>
+        <div className="box">
+          <div className="airdrop-date">
+            <span>3</span> Feb 2023
+          </div>
+          <div className="airdrop-name">TopGoal x CoinMarketCap 3rd NFTs Airdrop Event</div>
+          <div className="airdrop-amount">3 000 NFT</div>
+        </div>
+        <div className="box">
+          <div className="airdrop-date">
+            <span>3</span> Feb 2023
+          </div>
+          <div className="airdrop-name">TopGoal x CoinMarketCap 3rd NFTs Airdrop Event</div>
+          <div className="airdrop-amount">3 000 NFT</div>
+        </div>
+        <div className="box">
+          <div className="airdrop-date">
+            <span>3</span> Feb 2023
+          </div>
+          <div className="airdrop-name">TopGoal x CoinMarketCap 3rd NFTs Airdrop Event</div>
+          <div className="airdrop-amount">3 000 NFT</div>
+        </div>
+        <div className="box">
+          <div className="airdrop-date">
+            <span>3</span> Feb 2023
+          </div>
+          <div className="airdrop-name">TopGoal x CoinMarketCap 3rd NFTs Airdrop Event</div>
+          <div className="airdrop-amount">3 000 NFT</div>
+        </div>
+      </div>
+    </>
+  );
+};
