a8c1b827518a96847c9ddb5b8d0f5f5d14bd1312
shtamtails <maximyudin1@gmail.com>
2023-03-05 00:08:08 +0300

    Alert menu / Profile menu

--- src/assets/components/page/Header/Header.tsx
+++ src/assets/components/page/Header/Header.tsx
@@ -1,6 +1,8 @@
 import logo from "/src/assets/img/logo.png";
-import { FiBell, FiPlusCircle, FiSearch, FiMenu } from "react-icons/fi";
+import { FiBell, FiPlusCircle, FiSearch, FiMenu, FiSettings } from "react-icons/fi";
+import { MdExitToApp, MdLanguage } from "react-icons/md";
 import { BiChevronDown, BiGasPump } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
 import { Input } from "../../reusable/Input/Input";
 import { RefObject, useEffect, useRef, useState } from "react";
 import { ActionIcon } from "../../reusable/ActionIcon/ActionIcon";
@@ -10,6 +12,8 @@ import { Menu } from "../../reusable/Menu/Menu";
 import { MenuLabel } from "../../reusable/Menu/MenuLabel";
 import { MenuElement } from "../../reusable/Menu/MenuElement";
 import { Select, SelectData } from "../../reusable/Select/Select";
+import { MenuItem } from "../../reusable/Menu/MenuItem";
+import { MenuDivider } from "../../reusable/Menu/MenuDivider";
 const ethWeb3 = new Web3("https://mainnet.infura.io/v3/fab3eefeac694b8eb958f853c8920652");
 const optimismWeb3 = new Web3("https://optimism-mainnet.infura.io/v3/fab3eefeac694b8eb958f853c8920652");
 
@@ -27,16 +31,46 @@ const Header = () => {
     },
   ];
   const [value, setValue] = useState<string>("");
+
   const [gasPrice, setGasPrice] = useState<string | null>(null);
   const [baseFee, setBaseFee] = useState<string | null>("");
   const [isGasPriceOpened, setIsGasPriceOpened] = useState<boolean>(false);
-  const [gasPricesBtnRect, setGasPricesBtnRect] = useState<DOMRect | null>(null);
   const [displayedCryptoCurrency, setDisplayedCryptoCurrency] = useState<string>("ETH");
   const [selectedCryptoCurrency, setSelectedCryptoCurrency] = useState(data[0]);
 
+  const [gasPricesBtnRect, setGasPricesBtnRect] = useState<DOMRect | null>(null);
+
+  const [profileMenuBtnRect, setProfileMenuBtnRect] = useState<DOMRect | null>(null);
+  const [isProfileMenuOpened, setIsProfileMenuOpened] = useState(false);
+
+  const [alertMenuBtnRect, setAlertMenuBtnRect] = useState<DOMRect | null>(null);
+  const [isAlertMenuOpened, setIsAlertMenuOpened] = useState(false);
+
+  const gasPricesBtnRef: RefObject<HTMLAnchorElement> = useRef(null);
   const onGasPriceClick = () => {
     setIsGasPriceOpened(!isGasPriceOpened);
   };
+
+  const profileBtnRef: RefObject<HTMLAnchorElement> = useRef(null);
+  const onProfileBtnClick = () => {
+    setIsProfileMenuOpened(!isProfileMenuOpened);
+  };
+
+  const alertBtnRef: RefObject<HTMLAnchorElement> = useRef(null);
+  const onAlertBtnClick = () => {
+    setIsAlertMenuOpened(!isAlertMenuOpened);
+  };
+
+  useEffect(() => {
+    const gasBtnRect = gasPricesBtnRef.current?.getBoundingClientRect();
+    gasBtnRect && setGasPricesBtnRect(gasBtnRect);
+
+    const profileBtnRect = profileBtnRef.current?.getBoundingClientRect();
+    profileBtnRect && setProfileMenuBtnRect(profileBtnRect);
+
+    const alertBtnRect = alertBtnRef.current?.getBoundingClientRect();
+    alertBtnRect && setAlertMenuBtnRect(alertBtnRect);
+  }, []); // TODO: Set other way without state when component did mount
 
   const updateGasPrice = async () => {
     if (displayedCryptoCurrency === "ETH") {
@@ -58,13 +92,6 @@ const Header = () => {
       updateGasPrice();
     })();
   }, [displayedCryptoCurrency]);
-
-  const gasPricesBtnRef: RefObject<HTMLAnchorElement> = useRef(null);
-
-  useEffect(() => {
-    const gasBtnRect = gasPricesBtnRef.current?.getBoundingClientRect();
-    gasBtnRect && setGasPricesBtnRect(gasBtnRect);
-  }, [gasPrice]);
 
   // setInterval(async () => {
   //   updateGasPrice();
@@ -130,8 +157,48 @@ const Header = () => {
           </Menu>
         )}
         <ActionIcon icon={<FiPlusCircle size={22} />} className="main-icon pd-l-xl" />
-        <ActionIcon icon={<FiBell size={22} />} className="main-icon pd-l-xl" />
-        <ActionIcon img={logo} className="main-icon pd-l-xl" />
+        <ActionIcon
+          ref={alertBtnRef}
+          icon={<FiBell size={22} />}
+          className="main-icon pd-l-xl"
+          onClick={onAlertBtnClick}
+        />
+        {alertMenuBtnRect && (
+          <Menu
+            rect={alertMenuBtnRect}
+            width={300}
+            topOffset={15}
+            opened={isAlertMenuOpened}
+            setOpened={setIsAlertMenuOpened}
+            parentElementRef={alertBtnRef}
+          >
+            <>
+              <MenuLabel>Alerts</MenuLabel>
+            </>
+          </Menu>
+        )}
+        <ActionIcon ref={profileBtnRef} img={logo} className="main-icon pd-l-xl" onClick={onProfileBtnClick} />
+        {profileMenuBtnRect && (
+          <Menu
+            rect={profileMenuBtnRect}
+            width={180}
+            topOffset={15}
+            opened={isProfileMenuOpened}
+            setOpened={setIsProfileMenuOpened}
+            parentElementRef={profileBtnRef}
+          >
+            <>
+              <MenuLabel>Profile</MenuLabel>
+              <MenuItem icon={<CgProfile />}>Profile</MenuItem>
+              <MenuItem icon={<FiSettings />}>Settings</MenuItem>
+              <MenuItem icon={<MdLanguage />}>Language</MenuItem>
+              <MenuDivider />
+              <MenuItem icon={<MdExitToApp />} color="#E03130" hoverColor="#4C292D">
+                Log out
+              </MenuItem>
+            </>
+          </Menu>
+        )}
       </div>
     </div>
   );
